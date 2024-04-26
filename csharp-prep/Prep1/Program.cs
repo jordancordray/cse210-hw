5f9d8d814cf9648fbd7ec390343072d361443ea4
jordancordray <cor23024@byui.edu>
2024-04-26 08:19:24 -0600

    Prep 1 and Prep 2 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string FirstNameJC = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string LastNameJC = Console.ReadLine();
+        
+        Console.WriteLine($"\nYour name is {LastNameJC}, {FirstNameJC} {LastNameJC}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is your grade percentage? ");
+        string GradePercentageJC = Console.ReadLine();
+        int PercentageJC = int.Parse(GradePercentageJC);
+        
+        string LetterGradeJC = "";
+
+        if (PercentageJC >= 90)
+        {
+            LetterGradeJC = "A";
+        }
+        else if  (PercentageJC >= 80)
+        {
+            LetterGradeJC = "B";
+        }
+        else if  (PercentageJC >= 70)
+        {
+            LetterGradeJC = "C";
+        }
+        else if  (PercentageJC >= 60)
+        {
+            LetterGradeJC = "D";
+        }
+        else if (PercentageJC < 60)
+        {
+            LetterGradeJC = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {LetterGradeJC}");
+
+        if (PercentageJC > 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
+    } 
 }
