493c4a42185f228f0f0a56c72d76dd692b0c6ca2
jordancordray <cor23024@byui.edu>
2024-05-24 10:21:30 -0600

    fraction program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction F1 = new();
+        Console.WriteLine(F1.GetFractionString());
+        Console.WriteLine(F1.GetDecimalValue());
+        
+        Fraction F2 = new(5);
+        Console.WriteLine(F2.GetFractionString());
+        Console.WriteLine(F2.GetDecimalValue());
+        
+        Fraction F3 = new(3, 4);
+        Console.WriteLine(F3.GetFractionString());
+        Console.WriteLine(F3.GetDecimalValue());
+
+        Fraction F4 = new(1, 3);
+        Console.WriteLine(F4.GetFractionString());
+        Console.WriteLine(F4.GetDecimalValue());
+
+
     }
 }
