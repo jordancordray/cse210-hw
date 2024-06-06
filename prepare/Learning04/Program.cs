e26655d4300a8178604f56689c9bad2c8917ac25
jordancordray <cor23024@byui.edu>
2024-06-06 11:42:40 -0600

    Homework assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment sampleAssignment = new("Samuel Bennett", "Multiplication");
+        Console.WriteLine(sampleAssignment.GetSummary());
+    
+        MathAssignment math = new("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment writing = new("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
